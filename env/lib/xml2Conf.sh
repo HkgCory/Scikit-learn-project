7488e3790a83604d4f32ed3f8562a89c3f5407b5
HkgCory <hkgkenneth@gmail.com>
2023-02-26 17:39:54 -0500

    heart-disease-env-

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/kinhanglo/Desktop/machine-learning/Machine-Learning-environment/Scikit-learn-project/env/lib"
+XML2_LIBS="-lxml2 -L/Users/kinhanglo/Desktop/machine-learning/Machine-Learning-environment/Scikit-learn-project/env/lib -lz -L/Users/kinhanglo/Desktop/machine-learning/Machine-Learning-environment/Scikit-learn-project/env/lib -llzma -lpthread -L/Users/kinhanglo/Desktop/machine-learning/Machine-Learning-environment/Scikit-learn-project/env/lib -liconv -L/Users/kinhanglo/Desktop/machine-learning/Machine-Learning-environment/Scikit-learn-project/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/Users/kinhanglo/Desktop/machine-learning/Machine-Learning-environment/Scikit-learn-project/env/include/libxml2 -I/Users/kinhanglo/Desktop/machine-learning/Machine-Learning-environment/Scikit-learn-project/env/include"
+MODULE_VERSION="xml2-2.9.14"
+
---
+++ env/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/kinhanglo/Desktop/machine-learning/Machine-Learning-environment/Scikit-learn-project/env/lib"
+XSLT_LIBS="-lxslt -L/Users/kinhanglo/Desktop/machine-learning/Machine-Learning-environment/Scikit-learn-project/env/lib -lxml2 -L/Users/kinhanglo/Desktop/machine-learning/Machine-Learning-environment/Scikit-learn-project/env/lib -L/Users/kinhanglo/Desktop/machine-learning/Machine-Learning-environment/Scikit-learn-project/env/lib -lpthread -L/Users/kinhanglo/Desktop/machine-learning/Machine-Learning-environment/Scikit-learn-project/env/lib -liconv -L/Users/kinhanglo/Desktop/machine-learning/Machine-Learning-environment/Scikit-learn-project/env/lib -licui18n -licuuc -licudata -lm "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/kinhanglo/Desktop/machine-learning/Machine-Learning-environment/Scikit-learn-project/env/include"
+MODULE_VERSION="xslt-1.1.35"
